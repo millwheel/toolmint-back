d61c02d9832293f2819949e0ac40690164976a6a
sijun <sionwer5@gmail.com>
2025-06-22 15:17:09 +0900

    feat: create project

---
+++ src/main/java/com/parsimony/toolmint_back/ToolmintBackApplication.java
@@ -0,0 +1,13 @@
+package com.parsimony.toolmint_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolmintBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolmintBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parsimony/toolmint_back/ToolmintBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parsimony.toolmint_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolmintBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
